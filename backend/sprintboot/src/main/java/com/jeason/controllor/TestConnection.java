2891b42004b9c5db9e842ae5967c45238c187f2a
jeasonchan <439994526@qq.com>
2020-01-28 23:42:29 +0800

    update

---
+++ backend/sprintboot/src/main/java/com/jeason/controllor/TestConnection.java
@@ -0,0 +1,18 @@
+package com.jeason.controllor;
+
+import org.apache.coyote.Response;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestConnection {
+
+    @RequestMapping("/test1")
+    @ResponseBody //自动用response包装返回值，能否直接使用Response作为返回值？？
+    public String returnConnection() {
+        return "connectoin success!";
+    }
+
+
+}
