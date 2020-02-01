141fa606a7440652c3e0d387c3b49c6affc75241
jeasonchan <439994526@qq.com>
2020-02-01 17:20:59 +0800

    update

--- backend/sprintboot/src/main/java/com/jeason/controllor/TestConnection.java
+++ backend/sprintboot/src/main/java/com/jeason/controllor/TestConnection.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller
-//@ResponseBody //自动用response包装返回值，适用于方法和类，具体的
+//@ResponseBody
+// @ResponseBody注解表示该方法的返回的结果直接写入HTTP响应正文中（ResponseBody），一般在异步获取数据时使用，
+// 通常是在使用@RequestMapping后。
+// 不加该注解的情况下，返回值通常解析为跳转路径，加上@ResponseBody后返回结果不会被解析为跳转路径，而是直接写入HTTP响应正文中。
 
 @RestController  //直接用于代替上面的注解
 public class TestConnection {
