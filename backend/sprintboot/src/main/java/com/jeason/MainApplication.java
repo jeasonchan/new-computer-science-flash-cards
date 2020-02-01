63d3a659b998a1a2200f481e9dad9b424f445285
jeasonchan <439994526@qq.com>
2020-02-01 15:30:45 +0800

    update

--- backend/sprintboot/src/main/java/com/jeason/MainApplication.java
+++ backend/sprintboot/src/main/java/com/jeason/MainApplication.java
@@ -3,7 +3,7 @@ package com.jeason;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //运行该类的main方法来运行应用
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
