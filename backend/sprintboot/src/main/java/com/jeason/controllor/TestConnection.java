c34a042d3e533b51d7c1c0e2b0807a999fab45d3
jeasonchan <439994526@qq.com>
2020-02-01 17:19:12 +0800

    update

--- backend/sprintboot/src/main/java/com/jeason/controllor/TestConnection.java
+++ backend/sprintboot/src/main/java/com/jeason/controllor/TestConnection.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody //自动用response包装返回值，适用于方法和类，具体的
+
+@RestController  //直接用于代替上面的注解
 public class TestConnection {
 
     @RequestMapping(value = "/test1", method = RequestMethod.GET)
-    @ResponseBody //自动用response包装返回值，能否直接使用Response作为返回值？？
     public String returnConnection() {
         return "connectoin success!";
     }
